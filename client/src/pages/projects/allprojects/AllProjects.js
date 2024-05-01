5364bde94344ac2863e4f1cc30bc9cba5a98875c
Mustafa <c2k21106896@ms.pict.edu>
2024-05-01 21:12:54 +0530

    added rj police project

--- client/src/pages/projects/allprojects/AllProjects.js
+++ client/src/pages/projects/allprojects/AllProjects.js
@@ -4,6 +4,8 @@ import Card from 'components/Card/Card';
 import printezMain from '../../../assets/logo.png';
 import tataPowerMain from '../../../assets/tatapower.png';
 import tedxMain from '../../../assets/main.png';
+import rjpolice from "../../../assets//1706082969599.gif";
+
 
 export const AllProjects = () =>
 {
@@ -19,6 +21,10 @@ export const AllProjects = () =>
                 </div>
                 <div>
                     <Card id="card#3" title={`TEDxPICT Blogs Website`} description={"Visually appealing blogs website for TEDxPICT"} src={tedxMain} webLink={"https://blogs.tedxpict.in"} gitLink={"https://github.com/TechyMT/"} />
+                </div>
+                <div>
+                    <Card id="card#4" title={"Rj Police Cyber"} description={"A website for the Rajasthan Police Cyber Cell created for the Rajasthan Police Hackathon 1.0"} src={rjpolice} webLink={"https://rjpolice.netlify.app"} gitLink={"https://github.com/TechyMT/RJPOLICE_HACK_1620_CircuitSlingers_9"}
+                    />
                 </div>
 
             </div>
